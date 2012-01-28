db0e8d5c0388775333979d42b19abb19b249ab2f
Cristian Molina <rubylabs+megatux@bvision.com>
2012-01-28 00:59:16 -0300

    add some example callbacks

--- main.lua
+++ main.lua
@@ -6,14 +6,55 @@ function love.load()
   love.graphics.setFont(f)
   love.graphics.setColor(0,0,0,255)
   love.graphics.setBackgroundColor(255,255,255)
+
+  num = 0
+  dt = 0
+  gameIsPaused = false
 end
 
 function love.draw()
-  love.graphics.print("Hello World", 400, 300)
+  love.graphics.draw(image, imgx, imgy)
+  love.graphics.print("Click and drag the cake around or use the arrow keys", 10, 10)
 end
 
 function love.update(dt)
+  -- if gameIsPaused then return end
+
   if love.keyboard.isDown("up") then
     num = num + 100 * dt -- this would increment num by 100 per second
   end
+  if love.keyboard.isDown("escape") then
+    love.quit()
+    -- system.exit(0)
+  end
+end
+
+function love.mousepressed(x, y, button)
+   if button == 'l' then
+      imgx = x -- move image to where mouse clicked
+      imgy = y
+   end
+end
+
+function love.mousereleased(x, y, button)
+   if button == 'l' then
+      fireSlingshot(x,y)
+   end
+end
+
+function love.focus(f)
+  gameIsPaused = not f
+  if not f then
+    print("LOST FOCUS")
+  else
+    print("GAINED FOCUS")
+  end
+end
+
+function love.quit()
+  print("Thanks for playing! Come back soon!")
+end
+
+function fireSlingshot(x,y)
+  print("bum!")
 end
